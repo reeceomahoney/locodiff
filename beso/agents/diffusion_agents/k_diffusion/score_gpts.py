34c116c7808cc607c3f48f0958d895c5205433f0
Reece O'Mahoney <reece.omahoney3@gmail.com>
2024-01-25 13:35:04 +0000

    Tidy up forward method

--- beso/agents/diffusion_agents/k_diffusion/score_gpts.py
+++ beso/agents/diffusion_agents/k_diffusion/score_gpts.py
@@ -48,22 +48,12 @@ class CausalSelfAttention(nn.Module):
         self.n_head = n_heads
 
     def forward(self, x):
-        (
-            B,
-            T,
-            C,
-        ) = x.size()  # batch size, sequence length, embedding dimensionality (n_embd)
+        (B, T, C) = x.size()  # batch size, sequence length, embedding dimensionality (n_embd)
 
         # calculate query, key, values for all heads in batch and move head forward to be the batch dim
-        k = (
-            self.key(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
-        )  # (B, nh, T, hs)
-        q = (
-            self.query(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
-        )  # (B, nh, T, hs)
-        v = (
-            self.value(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
-        )  # (B, nh, T, hs)
+        k = (self.key(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2))  # (B, nh, T, hs)
+        q = (self.query(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2))  # (B, nh, T, hs)
+        v = (self.value(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2))  # (B, nh, T, hs)
 
         # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
         att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
@@ -71,9 +61,7 @@ class CausalSelfAttention(nn.Module):
         att = F.softmax(att, dim=-1)
         att = self.attn_drop(att)
         y = att @ v  # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
-        y = (
-            y.transpose(1, 2).contiguous().view(B, T, C)
-        )  # re-assemble all head outputs side by side
+        y = (y.transpose(1, 2).contiguous().view(B, T, C))  # re-assemble all head outputs side by side
 
         # output projection
         y = self.resid_drop(self.proj(y))
