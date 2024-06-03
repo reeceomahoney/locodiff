1323e8b45b7a05ad1abbec2789cfddee88858874
Reece O'Mahoney <reece.omahoney3@gmail.com>
2024-06-03 17:23:42 +0100

    Update nominal config

--- env/raisim/helpers/Observation.hpp
+++ env/raisim/helpers/Observation.hpp
@@ -19,7 +19,8 @@ private:
 
     int observationDim_ = 48;
     int historyLength_ = 2, nJoints_ = 12;
-    double nominalBaseHeight_ = 0.522;
+    double nominalBaseHeight_ = 0.59;
+    // double nominalBaseHeight_ = 0.285;
 
     Eigen::VectorXd nominalGeneralizedCoordinates_;
 
@@ -31,10 +32,14 @@ public:
         jointPositionErrorHistory_.setZero(historyLength_ * nJoints_);
 
         jointNominalConfig_.setZero(nJoints_);
-        jointNominalConfig_ << -0.089, 0.712, -1.03,
-                0.089, 0.712, -1.03,
-                -0.089, -0.712, 1.03,
-                0.089, -0.712, 1.03;
+        jointNominalConfig_ << -0.14, 0.81, -0.96,
+            0.14, 0.81, -0.96,
+            -0.14, -0.81, 0.96,
+            0.14, -0.81, 0.96;
+        // jointNominalConfig_ << -0.30, 1.90, -2.27,
+        //         0.30, 1.90, -2.27,
+        //         -0.30, -1.90, 2.27,
+        //         0.30, -1.90, 2.27;
 
         feetNominalPositions_.setZero(12);
         feetNominalPositions_ << 0.38, 0.27, -0.522,
