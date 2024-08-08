dc71dff7920c1c50f170758324ea82d43029c82a
Reece O'Mahoney <reece.omahoney3@gmail.com>
2024-08-08 18:27:33 +0100

    Update max velocities and scp script

--- scripts/utils/scp.py
+++ scripts/utils/scp.py
@@ -13,9 +13,9 @@ args = parser.parse_args()
 
 # Define the bash command
 if args.f:
-    cmd = f"scp xolotl:/home/reece/workspace/projects/locodiff/{args.f} {args.f}"
+    cmd = f"scp wanming:/home/reece/ws/projects/locodiff/{args.f} {args.f}"
 elif args.t:
-    cmd = f"scp -r xolotl:/home/reece/workspace/projects/locodiff/{args.t}/ {args.t}/"
+    cmd = f"scp -r wanming:/home/reece/ws/projects/locodiff/{args.t}/ {args.t}/"
 
 # Call the bash command
 subprocess.run(cmd, shell=True)
