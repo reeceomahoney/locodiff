Fix vel cmd calculation

env/raisim_env.py
@@ -66,11 +66,8 @@ class RaisimEnv:
         obs_and_cmd = self._observation[:, :36]
         obs_and_cmd = torch.from_numpy(obs_and_cmd).to(self.device)
         obs = obs_and_cmd[:, :33]
-        vel_cmd = obs_and_cmd[:, 33:36]
-
-        vel_cmd = torch.randint(0, 2, (self.num_envs, 1)).to(self.device)
-        vel_cmd = vel_cmd.float() * 2 - 1
-
+        # vel_cmd = obs_and_cmd[:, 33:36]
+        vel_cmd = self.vel_cmd
         return obs, vel_cmd
 
     def reset(self, conditional_reset=False):
@@ -92,6 +89,9 @@ class RaisimEnv:
         self.skill = torch.zeros(self.num_envs, self.skill_dim).to(self.device)
         self.skill[:, 0] = 1
         self.images = []
+
+        self.vel_cmd = torch.randint(0, 2, (self.num_envs, 1)).to(self.device)
+        self.vel_cmd = self.vel_cmd.float() * 2 - 1
 
         for _ in range(self.eval_n_times):
             self.env.reset()