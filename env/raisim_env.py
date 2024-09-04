Fix return calculation bug

env/raisim_env.py
@@ -62,8 +62,8 @@ class RaisimEnv:
         obs_and_cmd = self._observation[:, :36]
         obs_and_cmd = torch.from_numpy(obs_and_cmd).to(self.device)
         obs = obs_and_cmd[:, :33]
-        vel_cmd = obs_and_cmd[:, 33:36]
-        # vel_cmd = self.vel_cmd
+        # vel_cmd = obs_and_cmd[:, 33:36]
+        vel_cmd = self.vel_cmd
         return obs, vel_cmd
 
     def reset(self, conditional_reset=False):
@@ -78,15 +78,15 @@ class RaisimEnv:
     def simulate(self, agent, real_time=False, lambda_values=None):
         log.info("Starting trained model evaluation")
 
-        returns = torch.ones((self.num_envs, self.window, 1)).to(self.device)
+        returns = torch.ones((self.num_envs, 1)).to(self.device)
         self.skill = torch.zeros(self.num_envs, self.skill_dim).to(self.device)
         self.skill[:, 0] = 1
 
-        # self.vel_cmd = torch.randint(
-        #     0, 2, (self.num_envs, 1), device=self.device
-        # ).float()
+        self.vel_cmd = torch.randint(
+            0, 2, (self.num_envs, 1), device=self.device
+        ).float()
 
-        cond_lambdas = lambda_values if lambda_values is not None else [0, 1, 1.5, 2]
+        cond_lambdas = lambda_values if lambda_values is not None else [0, 10, 20, 50]
         assert self.num_envs % len(cond_lambdas) == 0
 
         # For parallel evaluation

locodiff/agent.py
@@ -309,7 +309,7 @@ class Agent:
     def load_pretrained_model(self, weights_path: str, **kwargs) -> None:
         self.model.load_state_dict(
             torch.load(
-                os.path.join(weights_path, "best_model_state_dict.pth"),
+                os.path.join(weights_path, "model_state_dict.pth"),
                 map_location=self.device,
             ),
             strict=False,
@@ -380,6 +380,8 @@ class Agent:
         skill = batch["skill"]
         vel_cmd = batch.get("vel_cmd", None)
         returns = batch.get("return", None)
+        if returns is None:
+            returns = self.compute_returns(raw_obs, vel_cmd)
 
         obs = self.scaler.scale_input(raw_obs[:, : self.T_cond])
 
@@ -395,10 +397,31 @@ class Agent:
             "action": action,
             "vel_cmd": vel_cmd,
             "skill": skill,
-            "return": returns[:, self.T_cond - 1] if returns is not None else None,
+            "return": returns,
         }
 
         return processed_batch
 
     def dict_to_device(self, batch):
         return {k: v.clone().to(self.device) for k, v in batch.items()}
+    
+    def compute_returns(self, obs, vel_cmd):
+        rewards = utils.reward_function(obs, vel_cmd, "vel_target")
+        rewards = rewards[:, self.T_cond - 1:] - 1
+
+        horizon = 50
+        gammas = torch.tensor([0.99**i for i in range(horizon)]).to(self.device)
+        returns = (rewards * gammas).sum(dim=-1)
+        returns = torch.exp(returns / 10)
+        returns += (1 - returns.max())
+
+        # import matplotlib.pyplot as plt
+
+        # plt.hist(returns.flatten().cpu().numpy(), bins=20)
+        # plt.xlabel("Returns")
+        # plt.ylabel("Frequency")
+        # plt.savefig("returns.png")
+        # exit()
+
+        return returns.unsqueeze(-1)
+        

locodiff/ddpm_agent.py
@@ -302,7 +302,7 @@ class Agent:
     def load_pretrained_model(self, weights_path: str, **kwargs) -> None:
         self.model.load_state_dict(
             torch.load(
-                os.path.join(weights_path, "best_model_state_dict.pth"),
+                os.path.join(weights_path, "model_state_dict.pth"),
                 map_location=self.device,
             ),
             strict=False,

locodiff/transformer.py
@@ -33,7 +33,7 @@ class DiffusionTransformer(nn.Module):
         self.action_emb = nn.Linear(self.act_dim, self.d_model)
         self.obs_emb = nn.Linear(self.obs_dim, self.d_model)
         self.sigma_emb = nn.Linear(1, self.d_model)
-        self.vel_cmd_emb = nn.Linear(3, self.d_model)
+        self.vel_cmd_emb = nn.Linear(1, self.d_model)
         self.return_emb = nn.Linear(1, self.d_model)
         self.skill_emb = nn.Linear(skill_dim, self.d_model)
 
@@ -169,13 +169,13 @@ class DiffusionTransformer(nn.Module):
         # embeddings
         action_emb = self.action_emb(noised_action)
         obs_emb = self.obs_emb(data_dict["obs"])
-        skill_emb = self.skill_emb(data_dict["skill"]).unsqueeze(1)
-        # vel_cmd_emb = self.vel_cmd_emb(data_dict["vel_cmd"]).unsqueeze(1)
+        # skill_emb = self.skill_emb(data_dict["skill"]).unsqueeze(1)
+        vel_cmd_emb = self.vel_cmd_emb(data_dict["vel_cmd"]).unsqueeze(1)
 
         returns = self.mask_cond(data_dict["return"], uncond)
         return_emb = self.return_emb(returns).unsqueeze(1)
 
-        cond = torch.cat([sigma_emb, return_emb, skill_emb, obs_emb], dim=1)
+        cond = torch.cat([sigma_emb, return_emb, vel_cmd_emb, obs_emb], dim=1)
         cond += self.cond_pos_emb
 
         action_emb += self.pos_emb

locodiff/utils.py
@@ -69,7 +69,7 @@ def reward_function(obs, vel_cmds, fn_name):
         vel_cmds = torch.tensor([0.8, 0.0, 0.0]).to(vel.device)
         # if vel.ndim == 3:
         #     vel_cmds = vel_cmds.unsqueeze(1)
-        rewards = torch.exp(-3*((vel - vel_cmds) ** 2)).mean(dim=-1)
+        rewards = torch.exp(-3 * ((vel - vel_cmds) ** 2)).mean(dim=-1)
     elif fn_name == "self_cmd":
         rewards = torch.zeros_like(x_vel)
         rewards = torch.where(z_vel >= 0, x_vel, rewards)