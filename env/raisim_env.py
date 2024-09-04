afb15557460e9ad67635f981fdf3ad4f02e4fe92
Reece O'Mahoney <reece.omahoney3@gmail.com>
2024-09-04 12:21:58 +0100

    Change model

--- env/raisim_env.py
+++ env/raisim_env.py
@@ -86,7 +86,7 @@ class RaisimEnv:
             0, 2, (self.num_envs, 1), device=self.device
         ).float()
 
-        cond_lambdas = lambda_values if lambda_values is not None else [0, 10, 20, 50]
+        cond_lambdas = lambda_values if lambda_values is not None else [0, 1, 2, 5]
         assert self.num_envs % len(cond_lambdas) == 0
 
         # For parallel evaluation
