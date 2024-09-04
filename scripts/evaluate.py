2cf3a0957e67f3812fdb1e4993131c947e0d4db8
Reece O'Mahoney <reece.omahoney3@gmail.com>
2024-09-04 18:29:04 +0100

    Add mse eval

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -74,81 +74,12 @@ def main(cfg: DictConfig) -> None:
         plt.xlabel("Lambda")
         plt.ylabel("Velocity tracking reward")
         plt.savefig("results.png")
-    else:
+    if cfg["test_mse"]:
         dataloader = agent.test_loader
         batch = next(iter(dataloader))
         batch = {k: v.to(cfg.device) for k, v in batch.items()}
-
-        if cfg["test_timestep_mse"]:
-            inference_steps = [1, 2, 3, 4, 5, 10, 20, 50]
-            results = []
-            for step in tqdm(inference_steps):
-                agent.num_sampling_steps = step
-                info = agent.evaluate(batch)
-                results.append(info["timestep_mse"].cpu().numpy())
-
-            for i, result in enumerate(results):
-                plt.plot(
-                    np.arange(1, len(result) + 1),
-                    result,
-                    label=f"{inference_steps[i]} inference steps",
-                )
-        if cfg["test_total_mse"]:
-            inference_steps = [1, 2, 3, 4, 5, 10, 20, 50]
-            results = []
-            for step in inference_steps:
-                agent.num_sampling_steps = step
-                info = agent.evaluate(batch)
-                results.append(info["total_mse"])
-            plt.plot(inference_steps, results, "x")
-        if cfg["test_observation_error"]:
-            info = agent.evaluate(batch)
-            results = info["mse"].cpu().numpy().mean(axis=(0, 1))
-            plt.bar(range(len(results)), results)
-        if cfg["visualize x-y trajectory"]:
-            agent.num_sampling_steps = 10
-            T_cond = model_cfg["T_cond"]
-            # batch = {k: v[:16] for k, v in batch.items()}
-
-            obs = batch["observation"].cpu().numpy()
-            obs[:, :, :2] -= obs[:, T_cond - 1 : T_cond, :2]
-
-            info = agent.evaluate(batch)
-            goal = info["goal"].cpu().numpy()
-            results = info["prediction"].cpu().numpy()
-
-            fig, axs = plt.subplots(4, 4, figsize=(15, 15))
-            axs = axs.flatten()
-
-            T_cond = model_cfg["T_cond"]
-            for i in range(16):
-                gt = obs[i, T_cond - 1 :, :2]
-                axs[i].plot(gt[:, 0], gt[:, 1], "o-", label="observed")
-                axs[i].plot(goal[i, 0], goal[i, 1], "rx", label="Goal")
-
-                gt_vel = obs[i, T_cond - 1 :, 33:36]
-                gt_ori = obs[i, T_cond - 1 :, 2:6]
-                gt_ori = np.roll(gt_ori, shift=-1, axis=1)
-                rot = R.from_quat(gt_ori).as_matrix()
-                gt_vel = np.einsum("bij, bj -> bi", rot, gt_vel)
-                for j in range(0, len(gt_vel), 10):
-                    axs[i].quiver(
-                        gt[j, 0],
-                        gt[j, 1],
-                        gt_vel[j, 0],
-                        gt_vel[j, 1],
-                        scale=5,
-                        width=0.005,
-                        color="r",
-                    )
-
-                pred = results[i, :, :2]
-                axs[i].plot(pred[:, 0], pred[:, 1], "x-", label="predicted")
-
-                axs[i].legend()
-
-            plt.tight_layout()
-            plt.savefig("results.png")
+        info = agent.evaluate(batch)
+        print(info["total_mse"])
 
 
 if __name__ == "__main__":
