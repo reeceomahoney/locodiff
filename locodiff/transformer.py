83113a56ec94a38f273f67f7d891e126e19a2533
Reece O'Mahoney <reece.omahoney3@gmail.com>
2024-09-07 13:29:29 +0100

    cmd in obs

--- locodiff/transformer.py
+++ locodiff/transformer.py
@@ -31,7 +31,7 @@ class DiffusionTransformer(nn.Module):
         self.ddpm = ddpm
 
         self.action_emb = nn.Linear(self.act_dim, self.d_model)
-        self.obs_emb = nn.Linear(self.obs_dim, self.d_model)
+        self.obs_emb = nn.Linear(self.obs_dim + 1, self.d_model)
         self.sigma_emb = nn.Linear(1, self.d_model)
         self.vel_cmd_emb = nn.Linear(1, self.d_model)
         self.return_emb = nn.Linear(1, self.d_model)
@@ -41,7 +41,7 @@ class DiffusionTransformer(nn.Module):
             SinusoidalPosEmb(d_model)(torch.arange(T)).unsqueeze(0).to(device)
         )
         self.cond_pos_emb = (
-            SinusoidalPosEmb(d_model)(torch.arange(T_cond + 3)).unsqueeze(0).to(device)
+            SinusoidalPosEmb(d_model)(torch.arange(T_cond + 2)).unsqueeze(0).to(device)
         )
 
         self.decoder = nn.TransformerDecoder(
@@ -168,14 +168,18 @@ class DiffusionTransformer(nn.Module):
 
         # embeddings
         action_emb = self.action_emb(noised_action)
-        obs_emb = self.obs_emb(data_dict["obs"])
+        obs = data_dict["obs"]
+        vel_cmd = data_dict["vel_cmd"]
+        obs = torch.cat([obs, vel_cmd], dim=1)
+        obs_emb = self.obs_emb(obs)
+        # obs_emb = self.obs_emb(data_dict["obs"])
         # skill_emb = self.skill_emb(data_dict["skill"]).unsqueeze(1)
         vel_cmd_emb = self.vel_cmd_emb(data_dict["vel_cmd"]).unsqueeze(1)
 
         returns = self.mask_cond(data_dict["return"], uncond)
         return_emb = self.return_emb(returns).unsqueeze(1)
 
-        cond = torch.cat([sigma_emb, return_emb, vel_cmd_emb, obs_emb], dim=1)
+        cond = torch.cat([sigma_emb, return_emb, obs_emb], dim=1)
         cond += self.cond_pos_emb
 
         action_emb += self.pos_emb
