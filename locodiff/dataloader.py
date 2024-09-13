d16b7bad54c4ffd260ac2e1e373e98e4d72387da
Reece O'Mahoney <reece.omahoney3@gmail.com>
2024-09-13 18:05:39 +0100

    Change to expert model

--- locodiff/dataloader.py
+++ locodiff/dataloader.py
@@ -84,7 +84,7 @@ class ExpertDataset(Dataset):
         processed_data = {
             "obs": obs,
             "action": actions,
-            # "vel_cmd": vel_cmds,
+            "vel_cmd": vel_cmds,
             "skill": skills,
             "mask": masks,
             # "reward": rewards,
