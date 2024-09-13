edd646f8470f5424eaca753e22ac36aac449fa77
Reece O'Mahoney <reece.omahoney3@gmail.com>
2024-09-13 18:40:42 +0100

    Update plotting script

--- scripts/figures/skill_switch.py
+++ scripts/figures/skill_switch.py
@@ -1,6 +1,5 @@
-import numpy as np
-
 import matplotlib.pyplot as plt
+import numpy as np
 
 plt.style.use("seaborn-v0_8")
 plt.rcParams.update({"xtick.labelsize": 24, "ytick.labelsize": 24})
@@ -14,7 +13,7 @@ x = np.arange(len(reward)) / 25
 window_size = 20
 reward_mean = np.convolve(reward, np.ones(window_size) / window_size, mode="valid")
 
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(14, 10), sharex=True)
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(36, 10), sharex=True)
 
 ax1.patch.set_edgecolor("black")
 ax1.patch.set_linewidth(1.5)
@@ -29,10 +28,17 @@ ax1.tick_params(labelbottom=True)
 
 ax2.plot(x, height, label="Height", color="red")
 ax2.set_xlabel("Time (s)", fontsize=24, labelpad=20)
-ax2.set_ylabel("Height", fontsize=24, labelpad=20)
+ax2.set_ylabel("Height (m)", fontsize=24, labelpad=20)
 
-ax1.set_title("Velocity Tracking Reward during skill change", fontsize=24, pad=20)
-legend = ax1.legend(frameon=True, fancybox=True, borderpad=0.5, facecolor="white", fontsize=24)
+ax1.set_title("Velocity tracking reward during skill change", fontsize=24, pad=20)
+
+# Add vertical line at x = 5 to both axes
+ax1.axvline(x=5, color="green", linestyle="--", linewidth=2, label="Skill Change")
+ax2.axvline(x=5, color="green", linestyle="--", linewidth=2)
+
+legend = ax1.legend(
+    frameon=True, fancybox=True, borderpad=0.5, facecolor="white", fontsize=24
+)
 legend.get_frame().set_linewidth(1.5)
 
 
