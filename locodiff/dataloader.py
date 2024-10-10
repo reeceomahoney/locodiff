fix vel cmd scaling

locodiff/dataloader.py
@@ -44,7 +44,7 @@ class ExpertDataset(Dataset):
     def load_and_process_data(self, dataset_path):
         data = np.load(dataset_path, allow_pickle=True).item()
 
-        obs = data["obs"][..., :33]
+        obs = data["obs"]
         actions = data["action"]
         vel_cmds = data["vel_cmd"]
         skills = data["skill"]
@@ -226,9 +226,8 @@ def get_dataloaders_and_scaler(
         # Build the scaler
         x_data = train_set.get_all_obs()
         # y_data = train_set.get_all_actions()
-        vel_cmds = train_set.get_all_vel_cmds().unsqueeze(1).expand(-1, x_data.shape[1], -1)
         y_data = torch.cat(
-            [train_set.get_all_obs(), vel_cmds, train_set.get_all_actions()], dim=-1
+            [train_set.get_all_obs(), train_set.get_all_actions()], dim=-1
         )
         scaler = Scaler(x_data, y_data, scaling, device)
 