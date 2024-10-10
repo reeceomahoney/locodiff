eb1328fcc41895c6aa890ea9d4ee7af2090f12d8
Reece O'Mahoney <reece.omahoney3@gmail.com>
2024-10-10 13:23:00 +0100

    add cmd to scaler

--- locodiff/dataloader.py
+++ locodiff/dataloader.py
@@ -226,8 +226,9 @@ def get_dataloaders_and_scaler(
         # Build the scaler
         x_data = train_set.get_all_obs()
         # y_data = train_set.get_all_actions()
+        vel_cmds = train_set.get_all_vel_cmds().unsqueeze(1).expand(-1, x_data.shape[1], -1)
         y_data = torch.cat(
-            [train_set.get_all_obs(), train_set.get_all_vel_cmds(), train_set.get_all_actions()], dim=-1
+            [train_set.get_all_obs(), vel_cmds, train_set.get_all_actions()], dim=-1
         )
         scaler = Scaler(x_data, y_data, scaling, device)
 
